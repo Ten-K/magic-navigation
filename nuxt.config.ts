50dfa24509b3a627e24091180f055c198cfbd1dd
lrl <742798240@qq.com>
2023-12-26 16:04:03 +0800

    fix: 删除高德地图相关引入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,16 +4,6 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	app: {
-		head: {
-			script: [
-				{
-					type: "text/javascript",
-					src: "https://webapi.amap.com/maps?v=2.0&key=c95bd7c58c6a3233adccce9a1b826685&plugin=AMap.Scale,AMap.HawkEye,AMap.ToolBar,AMap.ControlBar,AMap.Geocoder,AMap.MapType,AMap.CitySearch,AMap.Geolocation"
-				}
-			]
-		}
-	},
 	css: [
 		"element-plus/dist/index.css",
 		"element-plus/theme-chalk/dark/css-vars.css"

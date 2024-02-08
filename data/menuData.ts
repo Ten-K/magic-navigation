5601e5ec0e5062b54579e32946d374e6a1e491d4
lrl <742798240@qq.com>
2024-02-08 17:04:02 +0800

    perf: 使用静态资源替代在线icon

--- data/menuData.ts
+++ data/menuData.ts
@@ -1,5 +1,26 @@
 import { Menu as IconMenu } from "@element-plus/icons-vue";
 
+import es6 from "~/assets/images/es6.ico";
+import v2ex from "~/assets/images/v2ex.ico";
+import next from "~/assets/images/next.ico";
+import lobe from "~/assets/images/lobe.ico";
+import agedm from "~/assets/images/agedm.ico";
+import vueWebp from "~/assets/images/vue.webp";
+import github from "~/assets/images/github.png";
+import jsinfo from "~/assets/images/jsinfo.png";
+import rollup from "~/assets/images/rollup.png";
+import cupfox from "~/assets/images/cupfox.png";
+import kimiai from "~/assets/images/kimiai.ico";
+import oioweb from "~/assets/images/oioweb.ico";
+import caniuse from "~/assets/images/caniuse.png";
+import twitter from "~/assets/images/twitter.ico";
+import cssGrid from "~/assets/images/css-grid.ico";
+import yinhuadm from "~/assets/images/yinhuadm.png";
+import bilibiliApi from "~/assets/images/bilibiliApi.png";
+import tailwindcss from "~/assets/images/tailwindcss.ico";
+import reactRouter from "~/assets/images/react-router.png";
+import tailwindCheatSheet from "~/assets/images/tailwind-cheat-sheet.ico";
+
 const menuDataList = [
 	{
 		title: "Search",
@@ -11,22 +32,22 @@ const menuDataList = [
 		children: [
 			{
 				title: "掘金",
-				logo: "https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/6c61ae65d1c41ae8221a670fa32d05aa.svg",
+				icon: "juejin",
 				url: "https://juejin.cn"
 			},
 			{
-				title: "v2ex(科学上网)",
-				logo: "https://www.v2ex.com/static/favicon.ico",
-				url: "https://www.v2ex.com"
-			},
-			{
 				title: "GitHub",
-				logo: "https://github.githubassets.com/favicons/favicon.png",
+				logo: github,
 				url: "https://github.com"
 			},
 			{
+				title: "v2ex(科学上网)",
+				logo: v2ex,
+				url: "https://www.v2ex.com"
+			},
+			{
 				title: "Twitter(科学上网)",
-				logo: "https://abs.twimg.com/favicons/twitter.3.ico",
+				logo: twitter,
 				url: "https://twitter.com"
 			}
 		]
@@ -37,42 +58,42 @@ const menuDataList = [
 		children: [
 			{
 				title: "Vue",
-				logo: "https://img0.baidu.com/it/u=2696654273,9521079&fm=253&fmt=auto&app=138&f=PNG?w=400&h=400",
+				logo: vueWebp,
 				url: "https://cn.vuejs.org/guide/introduction.html"
 			},
 			{
 				title: "Vue Router",
-				logo: "https://img0.baidu.com/it/u=2696654273,9521079&fm=253&fmt=auto&app=138&f=PNG?w=400&h=400",
+				logo: vueWebp,
 				url: "https://router.vuejs.org/zh"
 			},
 			{
 				title: "Vuex",
-				logo: "https://img0.baidu.com/it/u=2696654273,9521079&fm=253&fmt=auto&app=138&f=PNG?w=400&h=400",
+				logo: vueWebp,
 				url: "https://vuex.vuejs.org/zh"
 			},
 			{
 				title: "VueUse",
-				logo: "https://vueuse.org/favicon.svg",
+				icon: "vueuse",
 				url: "https://vueuse.org"
 			},
 			{
 				title: "Pinia",
-				logo: "https://pinia.vuejs.org/logo.svg",
+				icon: "pinia",
 				url: "https://pinia.vuejs.org/zh"
 			},
 			{
 				title: "Nuxt3",
-				logo: "https://nuxt.com.cn/assets/design-kit/icon-green.svg",
+				icon: "nuxt3",
 				url: "https://nuxt.com.cn/docs/getting-started/introduction"
 			},
 			{
 				title: "Element Plus",
-				logo: "https://element-plus.org/images/element-plus-logo-small.svg",
+				icon: "element-plus",
 				url: "https://element-plus.org/zh-CN"
 			},
 			{
 				title: "Naive UI",
-				logo: "https://www.naiveui.com/assets/naivelogo-93278402.svg",
+				icon: "naive-ui",
 				url: "https://www.naiveui.com/zh-CN/os-theme"
 			}
 		]
@@ -88,22 +109,22 @@ const menuDataList = [
 			},
 			{
 				title: "React Router",
-				logo: "https://reactrouter.com/favicon-light.png",
+				logo: reactRouter,
 				url: "https://reactrouter.com/en/main"
 			},
 			{
 				title: "Redux",
-				logo: "https://redux.js.org/img/redux.svg",
+				icon: "redux",
 				url: "https://redux.js.org/introduction/getting-started"
 			},
 			{
 				title: "Next",
-				logo: "https://nextjs.org/favicon.ico",
+				logo: next,
 				url: "https://nextjs.org"
 			},
 			{
 				title: "Ant Design",
-				logo: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+				icon: "antd",
 				url: "https://ant-design.antgroup.com/index-cn"
 			}
 		]
@@ -114,27 +135,27 @@ const menuDataList = [
 		children: [
 			{
 				title: "UnoCss",
-				logo: "https://unocss.dev/logo.svg",
+				icon: "unocss",
 				url: "https://unocss.dev"
 			},
 			{
 				title: "Tailwindcss",
-				logo: "https://tailwindcss.com/favicons/favicon.ico?v=3",
+				logo: tailwindcss,
 				url: "https://tailwindcss.com"
 			},
 			{
 				title: "CSS Grid Generator",
-				logo: "https://cssgrid-generator.netlify.app/favicon.ico",
+				logo: cssGrid,
 				url: "https://cssgrid-generator.netlify.app/"
 			},
 			{
 				title: "Tailwind Css Cheat Sheet",
-				logo: "https://nerdcave.com/tailwind-cheat-sheet/favicon.ico",
+				logo: tailwindCheatSheet,
 				url: "https://nerdcave.com/tailwind-cheat-sheet"
 			},
 			{
 				title: "Can I use",
-				logo: "https://caniuse.com/img/favicon-128.png",
+				logo: caniuse,
 				url: "https://caniuse.com/"
 			}
 		]
@@ -145,12 +166,12 @@ const menuDataList = [
 		children: [
 			{
 				title: "现代 JavaScript 教程",
-				logo: "https://zh.javascript.info/img/favicon/favicon.png",
+				logo: jsinfo,
 				url: "https://zh.javascript.info"
 			},
 			{
 				title: "ES6 入门教程",
-				logo: "https://es6.ruanyifeng.com/favicon.ico",
+				logo: es6,
 				url: "https://es6.ruanyifeng.com"
 			},
 			{
@@ -171,17 +192,17 @@ const menuDataList = [
 			},
 			{
 				title: "Rollup",
-				logo: "https://cn.rollupjs.org/favicon.png",
+				logo: rollup,
 				url: "https://cn.rollupjs.org"
 			},
 			{
 				title: "Vite",
-				logo: "https://cn.vitejs.dev/logo.svg",
+				icon: "vite",
 				url: "https://cn.vitejs.dev"
 			},
 			{
 				title: "Vitest",
-				logo: "https://cn.vitest.dev/logo-shadow.svg",
+				icon: "vitest",
 				url: "https://cn.vitest.dev"
 			}
 		]
@@ -192,17 +213,17 @@ const menuDataList = [
 		children: [
 			{
 				title: "Age动漫",
-				logo: "https://www.agedm.org/favicon.ico",
+				logo: agedm,
 				url: "https://www.agedm.org"
 			},
 			{
 				title: "樱花动漫",
-				logo: "https://oss-cdn.meowa.cn/mxtheme/images/favicon.png",
+				logo: yinhuadm,
 				url: "https://www.yinhuadm.cc"
 			},
 			{
 				title: "茶杯狐影视",
-				logo: "https://picx.zhimg.com/80/v2-5393cb76a824b11d7771ecdce592c87d.png",
+				logo: cupfox,
 				url: "https://cupfox.love"
 			},
 			{
@@ -223,7 +244,7 @@ const menuDataList = [
 		children: [
 			{
 				title: "LobeChat(科学上网)",
-				logo: "https://registry.npmmirror.com/@lobehub/assets-favicons/1.4.0/files/assets/favicon.ico",
+				logo: lobe,
 				url: "https://chat.aivvm.com/chat"
 			},
 			{
@@ -233,7 +254,7 @@ const menuDataList = [
 			},
 			{
 				title: "Kimi",
-				logo: "https://kimi.moonshot.cn/favicon.ico",
+				logo: kimiai,
 				url: "https://kimi.moonshot.cn"
 			}
 		]
@@ -249,7 +270,7 @@ const menuDataList = [
 			},
 			{
 				title: "教书先生API",
-				logo: "https://api.oioweb.cn/favicon.ico",
+				logo: oioweb,
 				url: "https://api.oioweb.cn"
 			},
 			{
@@ -259,7 +280,7 @@ const menuDataList = [
 			},
 			{
 				title: "哔哩哔哩API",
-				logo: "https://socialsisteryi.github.io/bilibili-API-collect/assets/logo-bba15bcf.png",
+				logo: bilibiliApi,
 				url: "https://socialsisteryi.github.io/bilibili-API-collect"
 			}
 		]

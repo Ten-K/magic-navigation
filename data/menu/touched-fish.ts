c77c7c87d7a142adf23de1079a1a5b9dfd5916f5
lrl <742798240@qq.com>
2024-04-28 09:51:18 +0800

    feat: Component Party

--- data/menu/touched-fish.ts
+++ data/menu/touched-fish.ts
@@ -1,4 +1,5 @@
 import v2ex from "~/assets/images/v2ex.ico";
+import popper from '~/assets/svg/popper.svg';
 import github from "~/assets/images/github.png";
 import twitter from "~/assets/images/twitter.ico";
 import { Menu as IconMenu } from "@element-plus/icons-vue";
@@ -26,6 +27,11 @@ export default {
       title: "Twitter(科学上网)",
       logo: twitter,
       url: "https://twitter.com"
+    },
+    {
+      title: "Component Party",
+      logo: popper,
+      url: "https://component-party.dev/#reactivity.declare-state"
     }
   ]
 }
